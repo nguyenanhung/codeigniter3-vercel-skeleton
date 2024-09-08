cfd769ba06c16c8c9e7ea57013b626e299db395a
nguyenanhung <hungna.dev@gmail.com>
2024-09-08 15:15:19 +0700

    Add config cache main_cache_adapter

--- app/config/extend/admin_config.php
+++ app/config/extend/admin_config.php
@@ -8,7 +8,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
  * Time: 11:39
  */
 $config['admin_config_basic_auth'] = [
-    'serviceId' => 'My Blogspot Proxy Service',
+    'serviceId' => 'CodeIgniter skeleton on Vercel',
     'framework' => 'CodeIgniter v' . CI_VERSION,
     'username' => 'hungna',
     'password' => '20240908151234_3081d5b916d94eda017a273d720e116f1e622d67'
