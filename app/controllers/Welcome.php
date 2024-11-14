28297c861f92d95de23667579742f91022c5bace
nguyenanhung <hungna.dev@gmail.com>
2024-11-14 19:05:04 +0700

    Add debug pgsql

--- app/controllers/Welcome.php
+++ app/controllers/Welcome.php
@@ -53,4 +53,33 @@ class Welcome extends HungNG_CI_Base_Controllers
     {
         phpinfo();
     }
+
+    protected function pgsql_function_check($function_name = 'pg_connect'): string
+    {
+        if (function_exists($function_name)) {
+            return "function " . $function_name . " is already";
+        } else {
+            return "function " . $function_name . " is not available";
+        }
+    }
+
+    public function pgsql_check()
+    {
+        $data = array(
+            'function_check' => array(
+                'pg_connect' => $this->pgsql_function_check('pg_connect'),
+                'pg_close' => $this->pgsql_function_check('pg_close'),
+                'pg_query' => $this->pgsql_function_check('pg_query'),
+                'pg_escape_string' => $this->pgsql_function_check('pg_escape_string'),
+                'pg_last_error' => $this->pgsql_function_check('pg_last_error'),
+            )
+        );
+
+        $this->output
+            ->set_status_header()
+            ->set_content_type('application/json', 'utf-8')
+            ->set_output(json_encode($data, JSON_PRETTY_PRINT))
+            ->_display();
+        exit;
+    }
 }
